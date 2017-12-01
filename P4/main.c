aff5e5877f5d52ff0a7e18700a5e0930682dc639
Francisco Sousa <ist186416@tecnico.ulisboa.pt>
2017-12-01 23:22:36 +0000

    mais robusto sim!

--- P4/main.c
+++ P4/main.c
@@ -30,7 +30,7 @@ pthread_mutex_t mutex;
 pthread_cond_t barreira;
 
 /*--------------------------------------------------------------------
-| Type: args_t
+| * Type: args_t
 | Description: Estrutura com Informacao para Trabalhadoras
 ---------------------------------------------------------------------*/
 
@@ -44,7 +44,7 @@ typedef struct {
 	DoubleMatrix2D* matrix_aux;
 } args_t;
 
-int threads_on_wait = 0, iteracao = 0, periodo = 0, idproc = 0, go_maxD = 1, go_alarm = 0, go_int = 1;
+int threads_on_wait = 0, iteracao = 0, periodo = 0, go_maxD = 1, go_alarm = 0, go_int = 1;
 double max_max = 0;
 char* fichS,* tempFichS;
 DoubleMatrix2D* matrix_final;
@@ -61,19 +61,19 @@ void atualizaGoMaxD(double maxD) {
 	}
 	max_max = 0;
 }
-/*CTROLC A MEIO DO PRINT DA BUG*/
+
 void escreverFicheiroTemporario(){
 	FILE* apontoParaUmFile;
 	printf("SOU O %d A COMECAR A ESCREVER\n" , getpid() );
 	apontoParaUmFile = fopen(tempFichS,"w");
-
+	
 	if (apontoParaUmFile == NULL) {
 		perror(tempFichS);
 	}
-
+	
 	dm2dPrintToFile(matrix_final, apontoParaUmFile);
 	fclose(apontoParaUmFile);
-
+	
 	if (rename(tempFichS, fichS)) {
 		fprintf(stderr, "\nErro: Falha a renomear ficheiro.\n");
 		exit(-1);
@@ -93,31 +93,31 @@ void handlerSIGINT(int num) {
 		printf("SOU O %d e ALGUEM JA ESTA A MEIO\n", getpid());
 		wait(&estado);
 	} else {
-                printf("SOU O %d e FAZENDO NOVA COPIA\n", getpid());
-
+		printf("SOU O %d e FAZENDO NOVA COPIA\n", getpid());
+		
 		escreverFicheiroTemporario();
 	}
 	exit(0);
 }
 
 /*--------------------------------------------------------------------
-| Function: simulFatia
-| Description:	Funcao executada por cada tarefa trabalhadora a cada interacao.
-				Processa um bloco da matriz dada como argumento.
----------------------------------------------------------------------*/
+ | * Function: simulFatia
+ | Description:	Funcao executada por cada tarefa trabalhadora a cada interacao.
+ | Processa um bloco da matriz dada como argumento.
+ ---------------------------------------------------------------------*/
 
 DoubleMatrix2D* simulFatia(DoubleMatrix2D* matrix, DoubleMatrix2D* matrix_aux, int linhas, int colunas, int linha_ini, double maxD, double* pmax) {
 	int l, c;
 	double value, diff;
 	DoubleMatrix2D* act_matrix = matrix,* oth_matrix = matrix_aux;
-
+	
 	for (l = linha_ini; l < linha_ini + linhas; l++) {
 		for (c = 1; c < colunas +1; c++) {
 			value = (dm2dGetEntry(act_matrix, l-1, c) + dm2dGetEntry(act_matrix, l+1, c) + dm2dGetEntry(act_matrix, l, c-1) + dm2dGetEntry(act_matrix, l, c+1))/4.0;
-
+			
 			diff = fabs(value - dm2dGetEntry(act_matrix, l, c));
 			*pmax = omaior(*pmax, diff);
-
+			
 			dm2dSetEntry(oth_matrix, l, c, value);
 		}
 	}
@@ -125,13 +125,13 @@ DoubleMatrix2D* simulFatia(DoubleMatrix2D* matrix, DoubleMatrix2D* matrix_aux, i
 }
 
 /*--------------------------------------------------------------------
-| Function: parse_integer_or_exit
-| Description: Processa a string str, do parâmetro name, como um inteiro
----------------------------------------------------------------------*/
+ | * Function: parse_integer_or_exit
+ | Description: Processa a string str, do parâmetro name, como um inteiro
+ ---------------------------------------------------------------------*/
 
 int parse_integer_or_exit(char const *str, char const *name) {
 	int value;
-
+	
 	if (sscanf(str, "%d", &value) != 1) {
 		fprintf(stderr, "\nErro no argumento \"%s\".\n\n", name);
 		exit(1);
@@ -140,13 +140,13 @@ int parse_integer_or_exit(char const *str, char const *name) {
 }
 
 /*--------------------------------------------------------------------
-| Function: parse_double_or_exit
-| Description: Processa a string str, do parâmetro name, como um double
----------------------------------------------------------------------*/
+ | * Function: parse_double_or_exit
+ | Description: Processa a string str, do parâmetro name, como um double
+ ---------------------------------------------------------------------*/
 
 double parse_double_or_exit(char const *str, char const *name) {
 	double value;
-
+	
 	if (sscanf(str, "%lf", &value) != 1) {
 		fprintf(stderr, "\nErro no argumento \"%s\".\n\n", name);
 		exit(1);
@@ -154,19 +154,19 @@ double parse_double_or_exit(char const *str, char const *name) {
 	return value;
 }
 
-/*--------------------------------------------------------------------
-| Function: judaniWork
+/*-------------------------------------------------------------------
+| * Function: judaniWork
 | Description: Funcao executada por cada tarefa trabalhadora.
 |              Recebe como argumento uma estrutura do tipo args_t.
 --------------------------------------------------------------------*/
 
 void* slaveWork(void* a) {
-	int i, iteracoes, myid, n, klinhas, linha_ini, trabs, estado;
+	int i, iteracoes, myid, n, klinhas, linha_ini, trabs, estado, idproc;
 	double max_slave, maxD;
 	DoubleMatrix2D* matrix,* matrix_aux,* matrix_res;
 	args_t* args = (args_t*) a;
 	struct sigaction structIGN;
-
+	
 	/*Ler argumentos*/
 	myid = args->id;
 	n = args->colunas;
@@ -175,55 +175,52 @@ void* slaveWork(void* a) {
 	maxD = args->maxD;
 	matrix = args->matrix;
 	matrix_aux = args->matrix_aux;
-
-        char env[10];
-
+	
 	linha_ini = (klinhas * (myid-1)) + 1;
 	trabs = n/klinhas;
 	max_slave = 0;
-
+	
+	memset(&structIGN, 0, sizeof(structIGN));
+	structIGN.sa_handler = SIG_IGN;
+	
 	for (i = 0; i < iteracoes && go_maxD && go_int; i++) {
 		/*Calcular matriz*/
 		matrix_res = simulFatia(matrix, matrix_aux, klinhas, n, linha_ini, maxD, &max_slave);
-
+		
 		if (matrix_res == NULL) {
 			fprintf(stderr, "\nErro: Falha na simulacao.\n");
 			exit(-1);
 		}
 		matrix_aux = matrix;
 		matrix = matrix_res;
-
+		
 		if (pthread_mutex_lock(&mutex)) {
-		fprintf(stderr, "\nErro: Nao foi possivel obter o mutex.\n");
+			fprintf(stderr, "\nErro: Nao foi possivel obter o mutex.\n");
 			exit(-1);
 		}
-
+		
 		threads_on_wait++;
 		calcMaxMax(max_slave);
 		max_slave = 0;
-
+		
 		if (threads_on_wait == trabs) {
 			threads_on_wait = 0;
 			iteracao++;
 			atualizaGoMaxD(maxD);
-
+			
 			matrix_final = matrix_res;
 			if (go_alarm) printf("ALARME %ld\n", pthread_self());
-			errno = 0;
+			
 			if (go_alarm && !(go_alarm = 0) && waitpid(-1, &estado, WNOHANG)) {
-				snprintf(env, 10, "%d", i);
-
-				perror(env);
+				
 				idproc = fork();
-
+				
 				if (idproc == -1) {
-				printf("ERRO\n");
-
+					printf("Erro: Nao foi possivel criar um processo filho.\n");
+					
 				} else if (idproc == 0) {
 					/*Filho*/
 					
-					memset(&structIGN, 0, sizeof(structIGN));
-					structIGN.sa_handler = SIG_IGN;
 					if (sigaction(SIGINT, &structIGN, NULL) != 0) {
 						printf("Erro: \n");
 					}
@@ -232,15 +229,15 @@ void* slaveWork(void* a) {
 					exit(0);
 				} else {
 					/*Pai*/
-
+					
 				}
 			}
-
+			
 			if (pthread_cond_broadcast(&barreira)) {
 				fprintf(stderr, "\nErro: Falha a assinalar as condicoes.\n");
 				exit(-1);
 			}
-
+			
 		} else {
 			while (i >= iteracao) {
 				if (pthread_cond_wait(&barreira, &mutex)) {
@@ -253,13 +250,13 @@ void* slaveWork(void* a) {
 			fprintf(stderr, "\nErro: Nao foi possivel libertar o mutex.\n");
 			exit(-1);
 		}
-
+		
 	}
 	return NULL;
 }
 
 /*--------------------------------------------------------------------
-| Function: main
+| * Function: main
 | Description: Entrada do programa
 ---------------------------------------------------------------------*/
 
@@ -270,14 +267,14 @@ int main (int argc, char** argv) {
 	args_t* slave_args; pthread_t* slaves;
 	struct sigaction structSIGALRM, structSIGINT;
 	FILE* filep;
-        printf("SOU O PAI %d\n" , getpid() );
-
+	printf("SOU O PAI %d\n" , getpid() );
+	
 	if (argc != 11) {
 		fprintf(stderr, "\nNumero invalido de argumentos.\n");
 		fprintf(stderr, "Uso: heatSim N tEsq tSup tDir tInf iteracoes trabalhadoras maxD ficheiro periodo\n\n");
 		return 1;
 	}
-
+	
 	/* argv[0] = program name */
 	N = parse_integer_or_exit(argv[1], "N");
 	tEsq = parse_double_or_exit(argv[2], "tEsq");
@@ -289,56 +286,58 @@ int main (int argc, char** argv) {
 	maxD = parse_double_or_exit(argv[8], "diferenca_maxima");
 	fichS = argv[9];
 	periodoS = parse_integer_or_exit(argv[10], "periodo");
-
+	
 	/*Validar argumentos*/
 	if (N < 1 || tEsq < 0 || tSup < 0 || tDir < 0 || tInf < 0 || iteracoes < 1 || trab < 1 || N % trab != 0 || maxD < 0 || periodoS < 0 || !strlen(fichS)) {
 		fprintf(stderr, "\nErro: Argumentos invalidos.\n");
 		fprintf(stderr, "Uso: N >= 1, temperaturas >= 0,  trabalhadoras e iteracoes >= 1, diferenca maxima e periodo >= 0\n");
 		return 1;
 	}
-
+	
 	fprintf(stderr, "\nArgumentos:\n N=%d tEsq=%.1f tSup=%.1f tDir=%.1f tInf=%.1f iteracoes=%d trabalhadoras=%d maxD=%.2f ficheiro=%s periodo=%d\n", N, tEsq, tSup, tDir, tInf, iteracoes, trab, maxD, fichS, periodoS);
-
+	
 	/*Alocar slaves*/
 	slave_args = (args_t*) malloc(trab*sizeof(args_t));
-    slaves = (pthread_t*) malloc(trab*sizeof(pthread_t));
+	slaves = (pthread_t*) malloc(trab*sizeof(pthread_t));
 	tempFichS = (char*) malloc((strlen(fichS)+1)*sizeof(char));
+	memset(&structSIGALRM, 0, sizeof(structSIGALRM));
 	strcpy(tempFichS, fichS);
 	strcat(tempFichS, "~");
+	structSIGALRM.sa_handler = &handlerSIGALRM;
 
+	
 	/*Testa se existe o ficheiro*/
 	filep = fopen(fichS,"r");
 	if (filep) {
 		matrix = readMatrix2dFromFile(filep, N+2, N+2);
 		//dm2dPrint(matrix);
-                printf("printou\n");
+		printf("printou\n");
 		fclose(filep);
 	} else {
 		matrix = dm2dNew(N+2, N+2);
 	}
-
+	
 	/*Criar matriz aux*/
 	matrix_aux = dm2dNew(N+2, N+2);
-
+	
 	if (matrix == NULL || matrix_aux == NULL) {
 		fprintf(stderr, "\nErro: Nao foi possivel alocar memoria para as matrizes.\n\n");
 		return -1;
 	}
-
+	
 	/*Inicializar matriz*/
 	dm2dSetLineTo(matrix, 0, tSup);
 	dm2dSetLineTo(matrix, N+1, tInf);
 	dm2dSetColumnTo(matrix, 0, tEsq);
 	dm2dSetColumnTo(matrix, N+1, tDir);
-
+	
 	/*Repetir na auxiliar*/
 	dm2dCopy(matrix_aux, matrix);
-	memset(&structSIGALRM, 0, sizeof(structSIGALRM));
-	structSIGALRM.sa_handler = &handlerSIGALRM;
+	
 	if (sigaction(SIGALRM, &structSIGALRM, NULL) != 0) {
 		printf("Erro: \n");
 	}
-
+	
 	structSIGINT.sa_handler = &handlerSIGINT;
 	if (sigaction(SIGINT, &structSIGINT, NULL) != 0) {
 		printf("Erro: \n");
@@ -347,51 +346,49 @@ int main (int argc, char** argv) {
 	klinhas = N/trab;
 	periodo = periodoS;
 	alarm(periodo);
-
+	
 	for (i = 0; i < trab; i++) {
 		slave_args[i].id = i+1;
-	    slave_args[i].colunas = N;
+		slave_args[i].colunas = N;
 		slave_args[i].klinhas = klinhas;
 		slave_args[i].iter = iteracoes;
 		slave_args[i].maxD = maxD;
 		slave_args[i].matrix = matrix;
 		slave_args[i].matrix_aux = matrix_aux;
-	    if (pthread_create(&slaves[i], NULL, slaveWork, &slave_args[i])) {
+		if (pthread_create(&slaves[i], NULL, slaveWork, &slave_args[i])) {
 			fprintf(stderr, "\nErro: Um escravo falhou a criar.\n");
-	        return -1;
+			return -1;
 		}
 	}
 	printf("MAIN %ld\n", pthread_self());
-
+	
 	/*Terminar threads*/
 	for (i = 0; i < trab; i++) {
 		if (pthread_join(slaves[i], NULL)) {
 			fprintf(stderr, "\nErro: Um escravo falhou a terminar.\n");
-	        return -1;
+			return -1;
 		}
 	}
 	/*Apagar ficheiro*/
-	wait(NULL);
 	if ((filep = fopen(fichS,"r")) && !fclose(filep) && unlink(fichS) != 0) {
 		fprintf(stderr, "\nErro: Falhou a apagar o ficheiro.\n");
 		return -1;
 	}
-	perror("FINAL");
-
+	
 	/*Libertar e Imprimir*/
 	if (pthread_mutex_destroy(&mutex) != 0) {
 		fprintf(stderr, "\nErro: Falhou a destruir mutex.\n");
 		return -1;
 	}
-
+	
 	if (pthread_cond_destroy(&barreira) != 0) {
 		fprintf(stderr, "\nErro: Falhou a destruir variavel de condicao.\n");
 		return -1;
 	}
-
-	//dm2dPrint(matrix_final);
+	
+	dm2dPrint(matrix_final);
 	printf("printou\n");
-
+	
 	dm2dFree(matrix);
 	dm2dFree(matrix_aux);
 	free(tempFichS);
